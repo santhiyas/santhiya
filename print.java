2173e59d9feedc859982d0d92c690442da9eb465
santhiyas <santhiyasekar.97@gmail.com>
2017-06-13 18:46:39 +0530

    Create print.java

---
+++ print.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class print
+{ public static void main(String args[])
+{ 
+    int i;
+    Scanner s=new Scanner(System.in);
+   int n=s.nextInt();
+   int a[]=new int[n];
+   for(i=0;i<n;i++)
+   {System.out.println("hello");
+   }
+   }
+}
