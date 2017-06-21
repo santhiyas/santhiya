2448f97146b6a074db50483c5aa38eba621e4ee2
santhiyas <santhiyasekar.97@gmail.com>
2017-06-21 14:45:41 +0530

    Create evenno.java

---
+++ evenno.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+class evenno{
+    
+	public static void main (String[] args) 
+	{ 
+	   
+	    Scanner s=new Scanner(System.in);
+	 int s1=s.nextInt();
+	 int s2=s.nextInt();
+	   for(int i=s1;i<=s2;i++)
+	   {
+	           if(i%2==0)
+	           {
+	         System.out.println(i);
+	       }
+	   }
+	}
+}
