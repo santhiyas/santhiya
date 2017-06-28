6010276549266f27ecdb0618ea28547acba86c0c
santhiyas <santhiyasekar.97@gmail.com>
2017-06-28 10:39:35 +0530

    Create strtoint.java

---
+++ strtoint.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class strtoint
+{
+    public static void main(String args[])
+    {
+        String strTest="fed";
+        int iTest=Integer.parseInt(strTest);
+        System.out.println("actualString=" +strTest);
+        System.out.println("converted to Int=" + iTest);
+        
+    }
+}
